8f893f450441a9400298856739be4c10de4cdafa
Odin5din <133902344+Odin5din@users.noreply.github.com>
2023-07-10 20:23:56 +0900

    adsasd

--- lib/FourthTab.dart
+++ lib/FourthTab.dart
@@ -1,1 +1,17 @@
-class FourthTab {}
+import 'package:flutter/material.dart';
+
+class FourthTab extends StatelessWidget {
+  const FourthTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'First Tab',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}

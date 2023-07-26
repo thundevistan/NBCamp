6bac5e301ccea4aec56fa29a74a0dcbf2ee72802
minji05 <smj95133@likelion.org>
2023-07-26 17:22:17 +0900

    [Add] Snacks 파일 수정

--- app/src/main/java/com/example/nbcamp_week3_assignment/Snacks.kt
+++ app/src/main/java/com/example/nbcamp_week3_assignment/Snacks.kt
@@ -1,14 +1,23 @@
 package com.example.nbcamp_week3_assignment
 
-class Snacks() : Menu() {
+class Snacks : Menu() {
     override fun add(orderList: MutableSet<String>) {
         println("[ Snacks MENU ]")
-        println("1. Snacks 1 | 간단한 설명")
-        println("2. Snacks 2 | 간단한 설명")
-        println("3. Snacks 3 | 간단한 설명")
-        println("4. Snacks 4 | 간단한 설명")
+        println("1. 대전부르스약과 | 간단한 설명")
+        println("2. 쑥떡앙빵 | 간단한 설명")
+        println("3. 파래미니전병 | 간단한 설명")
+        println("4. 땅콩미니전병 | 간단한 설명")
+        println("5. 대전부르스만주 1호 | 간단한 설명")
 
-        var choice = readln()!!.toInt()
-        orderList.add(snacksArray[choice-1][0].toString())
+        var choice = readLine()?.toIntOrNull()
+
+        when (choice) {
+            1 -> orderList.add("대전부르스약과 | 1200")
+            2 -> orderList.add("쑥떡앙빵 | 1800")
+            3 -> orderList.add("파래미니전병 | 3500")
+            4 -> orderList.add("땅콩미니전병 | 1000")
+            5 -> orderList.add("대전부르스만주 1호 | 17000")
+            else -> println("잘못된 선택입니다.")
+        }
     }
 }

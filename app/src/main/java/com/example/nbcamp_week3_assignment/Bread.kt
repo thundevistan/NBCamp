3acc22e91589887e762b69b2810051152bf6139e
minji05 <smj95133@likelion.org>
2023-07-26 17:23:25 +0900

    [Add] Bread 파일 수정

--- app/src/main/java/com/example/nbcamp_week3_assignment/Bread.kt
+++ app/src/main/java/com/example/nbcamp_week3_assignment/Bread.kt
@@ -1,15 +1,22 @@
 package com.example.nbcamp_week3_assignment
-
-class Bread() : Menu() {
+class Bread : Menu() {
     override fun add(orderList: MutableSet<String>) {
         println("[ Bread MENU ]")
-        println("1. 소보루빵 | 간단한 설명")
-        println("2. 단팥빵 | 간단한 설명")
-        println("3. 마늘빵 | 간단한 설명")
-        println("4. 맘모스빵 | 간단한 설명")
+        println("1. 튀소패밀리 | 간단한 설명")
+        println("2. 파이만주세트 | 간단한 설명")
+        println("3. 오키도키슈 | 간단한 설명")
+        println("4. 오키도키슈세트 | 간단한 설명")
+        println("5. 김과장세트 | 간단한 설명")
 
-        var choice = readln()!!.toInt()
-        orderList.add(breadArray[choice-1][0].toString())
-        //테스트
+        var choice = readLine()?.toIntOrNull()
+
+        when (choice) {
+            1 -> orderList.add("튀소패밀리 | 10800")
+            2 -> orderList.add("파이만주세트 | 9600")
+            3 -> orderList.add("오키도키슈 | 3500")
+            4 -> orderList.add("오키도키슈세트 | 14000")
+            5 -> orderList.add("김과장세트 | 40000")
+            else -> println("잘못된 선택입니다.")
+        }
     }
 }

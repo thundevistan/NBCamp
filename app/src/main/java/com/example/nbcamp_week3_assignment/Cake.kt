825dbd1f5436e0976a6bb3327c3b1550305eede1
minji05 <smj95133@likelion.org>
2023-07-26 17:23:01 +0900

    [Add] Cake 파일 수정

--- app/src/main/java/com/example/nbcamp_week3_assignment/Cake.kt
+++ app/src/main/java/com/example/nbcamp_week3_assignment/Cake.kt
@@ -1,14 +1,23 @@
 package com.example.nbcamp_week3_assignment
 
-class Cake() : Menu() {
+class Cake : Menu() {
     override fun add(orderList: MutableSet<String>) {
         println("[ Cake MENU ]")
-        println("1. Cake 1 | 간단한 설명")
-        println("2. Cake 2 | 간단한 설명")
-        println("3. Cake 3 | 간단한 설명")
-        println("4. Cake 4 | 간단한 설명")
+        println("1. 축복 4호 | 간단한 설명")
+        println("2. 키리쉬모짜르트 | 간단한 설명")
+        println("3. 티라미수 1호 | 간단한 설명")
+        println("4. 딸기요거트 | 간단한 설명")
+        println("5. 클래식치즈케이크 | 간단한 설명")
 
-        var choice = readln()!!.toInt()
-        orderList.add(cakeArray[choice-1][0].toString())
+        var choice = readLine()?.toIntOrNull()
+
+        when (choice) {
+            1 -> orderList.add("축복 4호 | 41000")
+            2 -> orderList.add("키리쉬모짜르트 | 35000")
+            3 -> orderList.add("티라미수 1호 | 30000")
+            4 -> orderList.add("딸기요거트 | 30000")
+            5 -> orderList.add("클래식치즈케이크 | 30000")
+            else -> println("잘못된 선택입니다.")
+        }
     }
 }

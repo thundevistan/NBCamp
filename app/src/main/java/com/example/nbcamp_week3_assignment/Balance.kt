37f88b70d9f3f59eb4f1c130271bb8e4ff31ca58
추민수 <devsoftychoo@gmail.com>
2023-07-26 16:15:46 +0900

    [FEAT] : ⚙️ 현재잔액 랜덤으로 부여하는 기능 추가
    Random 함수를 이용하여 클래스 구현완료
    rand(a,b)함수를 호출하는 경우에 a,b 값을 정하여 호출 할 경우 ex) rand(10,15)
    10~15 사이의값이 랜덤으로 변수에 저장됨

---
+++ app/src/main/java/com/example/nbcamp_week3_assignment/Balance.kt
@@ -0,0 +1,12 @@
+package com.example.nbcamp_week3_assignment
+
+import java.util.Random
+
+
+class Balance {
+    val random = Random()
+    fun rand(from : Int, to : Int): Int {
+        return random.nextInt(to-from)+from
+    }
+
+}

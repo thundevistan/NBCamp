8fb3a3f0a15ba9097bcaa7a43f7d80c14693433b
minji05 <smj95133@likelion.org>
2023-07-26 17:25:19 +0900

    [Add] Main 파일 수정

--- app/src/main/java/com/example/nbcamp_week3_assignment/main.kt
+++ app/src/main/java/com/example/nbcamp_week3_assignment/main.kt
@@ -1,51 +1,40 @@
 package com.example.nbcamp_week3_assignment
+
+import java.text.DecimalFormat
+
 fun main() {
     val orderList: MutableSet<String> = mutableSetOf()
     val bread = Bread()
     val snacks = Snacks()
     val cake = Cake()
-    var menu = Menu()
+    val order = Order() // Order 클래스 인스턴스 생성
+    var currentBalance = 50000 // 임의로 5만원으로 설정
     var n3: Int = 0
-    //var account = Balance().rand(20,30)
-
     println("성심당에 오신것을 환영합니다.")
-   // println("현재 잔액은 ${account}만원 입니다.")
-
-    Datetime().inspection() //은행결제시간과 현재 시간을 받아오는 코드입니다 나중에 옮겨서 사용하시면 됩니다:)
+    Datetime().inspection() // 은행결제시간과 현재 시간을 받아오는 코드입니다 나중에 옮겨서 사용하시면 됩니다:)
 
     while (true) {
         while (true) {
-            try {
-                println("주문하시겠습니까? [yes : 1] [no : 2] ")
-                var n = readln()!!.toInt()
-                when (n) {
-                    1 -> {
-                        println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.\n")
-                        println("[ 성심당 MENU ]")
-                        println("1. Bread | 간단한 설명")
-                        println("2. Snacks | 간단한 설명")
-                        println("3. Cake | 간단한 설명")
-                        println("4. Roll & Pound | 간단한 설명")
-                        break
-                    }
+            println("주문하시겠습니까? [yes : 1] [no : 2] ")
+            val n = readLine()?.toIntOrNull()
 
-                    2 -> {
-                        println("키오스크를 종료합니다.")
-                        return
-                    }
-
-                    else -> {
-                        println("1 또는 2의 숫자만 입력할 수 있습니다")
-                        continue
-                    }
-                }
-            } catch (e: java.lang.NumberFormatException) {
-                println("잘못 입력하셨습니다. 숫자를 입력해 주세요")
+            if (n == 1) {
+                println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.\n")
+                println("[ 성심당 MENU ]")
+                println("1. Bread | 간단한 설명")
+                println("2. Snacks | 간단한 설명")
+                println("3. Cake | 간단한 설명")
+                println("4. Roll & Pound | 간단한 설명")
+                break
+            } else if (n == 2) {
+                println("키오스크를 종료합니다.")
+                return
+            } else {
+                println("1 또는 2의 숫자만 입력할 수 있습니다")
             }
         }
 
-        var n2 = readln()!!.toInt()
-        var choice: String
+        val n2 = readLine()?.toIntOrNull()
         when (n2) {
             1 -> bread.add(orderList)
             2 -> snacks.add(orderList)
@@ -54,35 +43,24 @@ fun main() {
             else -> println("예외처리")
         }
         println("장바구니에 추가되었습니다!")
+
         while (true) {
-            try {
-                println("[더 주문하기 : 1] [결제 : 2]")
-                n3 = readln()!!.toInt()
-                when (n3) {
-                    1 -> {
-                        break
-                        continue
-                    }
+            println("[더 주문하기 : 1] [결제 : 2]")
+            val n3 = readLine()?.toIntOrNull()
 
-                    2 -> {
-                        break
-                    }
-
-                    else -> {
-                        println("1 또는 2의 숫자만 입력할 수 있습니다")
-                        continue
-                    }
+            when (n3) {
+                1 -> break
+                2 -> {
+                    order.showOrderDetails(orderList, currentBalance) // 주문 내역과 결제 기능 호출
+                    break
                 }
-            } catch (e: java.lang.NumberFormatException) {
-                println("잘못 입력하셨습니다. 숫자를 입력해 주세요")
+                else -> println("1 또는 2의 숫자만 입력할 수 있습니다")
             }
-
         }
-        when (n3) {
-            2 -> break
+        if (n3 == 2) {
+            break
         }
     }
 
-    println(orderList) //*출력대신 Order class 호출 예정* -> 호출 시 orderList를 함께 넘겨주기
-
+    // println(orderList) //*출력대신 Order class 호출 예정* -> 호출 시 orderList를 함께 넘겨주기
 }

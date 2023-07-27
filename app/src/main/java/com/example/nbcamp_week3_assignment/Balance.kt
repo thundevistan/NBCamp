3c841321fb9a57f88f418f801b0e9efb962b3a24
추민수 <devsoftychoo@gmail.com>
2023-07-27 09:38:35 +0900

    [FEAT] : ⚙️ 초기자금 범위 재설정
    초기에 20~30만원 사이로 구현돼있던 기능
    -> ex) 234088 구현은 되어있지만 1원단위까지 표시됨
    ---------------
    변경사항 : 동일하게 20~30만원 사이로 출력은 가능하되
    만원단위 이하의 자릿수는 날림
    ex) 210000 or 280000 등으로 표시

--- app/src/main/java/com/example/nbcamp_week3_assignment/Balance.kt
+++ app/src/main/java/com/example/nbcamp_week3_assignment/Balance.kt
@@ -6,7 +6,10 @@ import java.util.Random
 class Balance {
     val random = Random()
     fun rand(from: Int, to: Int): Int {
-        return random.nextInt(to - from) + from
+//        return random.nextInt(to - from) + from
+        var balance = random.nextInt(to - from) + from
+        return balance * 10000
+
     }
 
 }

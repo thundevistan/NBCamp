3460658ccdfd3ae77e2bbbe10c5de082956c064f
KotDev99 <KotDev99@gmail.com>
2023-08-06 01:27:58 +0900

    🎉 choice assignment clear!! 🎉

--- app/src/main/java/com/android/introduceapp/SignUpActivity.kt
+++ app/src/main/java/com/android/introduceapp/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -23,14 +24,17 @@ class SignUpActivity : AppCompatActivity() {
 			val editText_paasword = findViewById<EditText>(R.id.editText_password)
 			val strPassword = editText_paasword.text.toString()
 
-			var isName = strName.isEmpty()          // 이름 텍스트가 비어있는지 확인, 초기값 : true
-			var isId = strId.isEmpty()              // 아이디 텍스트가 비어있는지 확인, 초기값 : true
-			var isPassword = strPassword.isEmpty()  // 비밀번호 텍스트가 비어있는지 확인, 초기값 : true
+			val isName = strName.isEmpty()          // 이름 텍스트가 비어있는지 확인, 초기값 : true
+			val isId = strId.isEmpty()              // 아이디 텍스트가 비어있는지 확인, 초기값 : true
+			val isPassword = strPassword.isEmpty()  // 비밀번호 텍스트가 비어있는지 확인, 초기값 : true
 
 			// 이름, 이아디, 비밀번호 모두 입력 시 finish() 호출
 			if (isName || isId || isPassword) {
 				Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
 			} else {
+				intent.putExtra("id", strId)        // 아이디 전달
+				intent.putExtra("password", strPassword)        // 비밀번호 전달
+				setResult(RESULT_OK, intent)        // RESULT_OK 전달하여 ActivityResult 수신 구문 실행
 				finish()
 			}
 		}

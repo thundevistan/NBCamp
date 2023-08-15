eca8adc85b0ecbae61185418880b11a3c3ee676c
KotDev99 <KotDev99@gmail.com>
2023-08-15 23:29:48 +0900

    [FEAT] : ⚙️메인 페이지 Layout 틀 구현 완료

---
+++ app/src/main/java/com/example/android_supporters_sns_project/MainPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_supporters_sns_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPageActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main_page)
+	}
+}

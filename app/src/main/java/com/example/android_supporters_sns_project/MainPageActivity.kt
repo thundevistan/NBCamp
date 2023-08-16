36e9b6fdfe930b211258ad336186e1303e71ac77
KotDev99 <KotDev99@gmail.com>
2023-08-16 09:11:19 +0900

    [FEAT] : ⚙️메인 페이지 Post 부분 addView로 구현

--- app/src/main/java/com/example/android_supporters_sns_project/MainPageActivity.kt
+++ app/src/main/java/com/example/android_supporters_sns_project/MainPageActivity.kt
@@ -1,11 +1,21 @@
 package com.example.android_supporters_sns_project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainPageActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main_page)
+
+		val container = findViewById<LinearLayout>(R.id.main_addView_container)
+		val inflater = LayoutInflater.from(this@MainPageActivity)
+
+		for (i in 0..4) {
+			val mainPost = inflater.inflate(R.layout.activity_main_page_post, null)
+			container.addView(mainPost)
+		}
 	}
 }

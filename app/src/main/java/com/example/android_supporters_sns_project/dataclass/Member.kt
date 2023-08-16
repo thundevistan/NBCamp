01e1100cca2fdb3048b42b19729ac137867973b4
ars-yeon <zosel7301@gmail.com>
2023-08-16 20:26:48 +0900

    [FEAT] : ⚙️ - 멤버 관리를 위한 'MemberManager' 객체 정의 (List 포함) - 멤버 등록 기능 구현 및 MemberManager에 멤버 추가

--- app/src/main/java/com/example/android_supporters_sns_project/dataclass/Member.kt
+++ app/src/main/java/com/example/android_supporters_sns_project/dataclass/Member.kt
@@ -1,8 +1,8 @@
 package com.example.android_supporters_sns_project.dataclass
 
 data class Member(
-    val email : String,
-    val password : String,
-    val name : String,
-    val nickname : String
+    val email: String,
+    val password: String,
+    val name: String,
+    val nickname: String
 )

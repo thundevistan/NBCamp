49b1998f1e1f9c3ede34f83a2b9bf89701fe51aa
ars-yeon <zosel7301@gmail.com>
2023-08-16 16:36:36 +0900

    [FEAT] : ⚙️ - 갤러리 권한 요청 (허용/거부) - 갤러리에서 이미지 선택 시 프로필 이미지 변경

--- app/src/main/java/com/example/android_supporters_sns_project/ModifyInfoActivity.kt
+++ app/src/main/java/com/example/android_supporters_sns_project/ModifyInfoActivity.kt
@@ -1,19 +1,124 @@
 package com.example.android_supporters_sns_project
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputLayout
 
 class ModifyInfoActivity : AppCompatActivity() {
+
+    lateinit private var editImage: ImageView
+    lateinit private var editImageButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modify_info)
 
+        initImageView()
+
         passwordTextWatcher()
+    }
+
+    // 이미지 수정 버튼을 눌렀을때 실행되는 함수
+    private fun initImageView() {
+        editImage = findViewById(R.id.modify_info_image)
+        editImageButton = findViewById(R.id.modify_info_image_edit)
+
+        editImageButton.setOnClickListener {
+            when {
+                // 갤러리 접근 권한이 있는 경우
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    navigateGallery()
+                }
+                // 갤러리 접근 권한이 없는 경우
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_MEDIA_IMAGES)
+                -> {
+                    showPermissionContextPopup()
+                }
+
+                // 권한 요청(requestPermissions) -> 갤러리 접근(onRequestPermissionResult)
+                else -> requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1000
+                )
+            }
+
+
+        }
+    }
+
+    // 권한 요청 승인 이후 실행되는 함수
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    navigateGallery()
+                else
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+            else -> {
+            }
+        }
+    }
+
+    // 갤러리에서 사진만 가져옴
+    private fun navigateGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        pickImageActivityResult.launch(intent)
+    }
+
+    // 갤러리에서 사진 선택한 후
+    private val pickImageActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                if (data != null) {
+                    val selectedImageUri: Uri? = data.data
+                    if (selectedImageUri != null) {
+                        editImage.setImageURI(selectedImageUri)
+                    } else {
+                        Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+    // 권한 요청 팝업
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("프로필 이미지를 바꾸기 위해서는 갤러리 접근 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
     }
 
     private fun isPasswordValid(password: String): Boolean {
@@ -21,20 +126,25 @@ class ModifyInfoActivity : AppCompatActivity() {
     }
 
     private fun passwordTextWatcher() {
-        val passwordLayout = findViewById<TextInputLayout>(R.id.modify_info_password_textinputlayout)
+        val passwordLayout =
+            findViewById<TextInputLayout>(R.id.modify_info_password_textinputlayout)
         val errorMessageView = findViewById<TextView>(R.id.modify_info_password_error_message)
 
         passwordLayout.editText?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
+
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if ((s?.length ?: 0) in 1..7 || (s?.length ?: 0) > 16 || !isPasswordValid(s.toString())) {
+                if ((s?.length ?: 0) in 1..7 || (s?.length
+                        ?: 0) > 16 || !isPasswordValid(s.toString())
+                ) {
                     errorMessageView.visibility = View.VISIBLE
                     errorMessageView.text = getString(R.string.modifyinfo_password_warning_text)
                 } else {
                     errorMessageView.visibility = View.INVISIBLE
                 }
             }
+
             override fun afterTextChanged(s: Editable?) {
             }
         })

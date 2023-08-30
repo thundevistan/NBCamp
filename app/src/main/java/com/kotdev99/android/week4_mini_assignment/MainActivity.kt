edd3e538224626892ce02c4292ee407229d40667
KotDev99 <KotDev99@gmail.com>
2023-08-30 18:14:47 +0900

    Initial commit

---
+++ app/src/main/java/com/kotdev99/android/week4_mini_assignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kotdev99.android.week4_mini_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}

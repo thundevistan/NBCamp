74b8909daa4d5828f4560dc32aa823b7747ba4bf
hyunsik jung <ball1241@naver.com>
2023-09-07 11:23:27 +0900

    [FEAT]:Detail Activity에서 문자와 전화 연결하기, MyPage에서 Edit기능 추가

--- app/src/main/java/com/example/contract/DetailActivity.kt
+++ app/src/main/java/com/example/contract/DetailActivity.kt
@@ -1,9 +1,14 @@
 package com.example.contract
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.contract.R
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,5 +34,34 @@ class DetailActivity : AppCompatActivity() {
         phoneNumberTextView.text = phoneNumber
         emailTextView.text = email
         eventTextView.text = event
+
+        // 전화 걸기 버튼에 클릭 리스너 추가
+        val callButton = findViewById<Button>(R.id.btn_call)
+        callButton.setOnClickListener {
+            // 전화 번호를 가져와서 전화 걸기 인텐트 생성
+            val phone = phoneNumberTextView.text.toString()
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$phone")
+
+            // 전화 걸기 권한이 있는지 확인
+            if (checkSelfPermission(android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 있으면 전화 걸기 인텐트 실행
+                startActivity(intent)
+            } else {
+                // 권한이 없으면 권한 요청
+                requestPermissions(arrayOf(android.Manifest.permission.CALL_PHONE), 1)
+            }
+        }
+
+        val messageButton = findViewById<Button>(R.id.btn_message)
+        messageButton.setOnClickListener {
+            // 전화 번호를 가져와서 문자 메시지 보내기 인텐트 생성
+            val phone = findViewById<TextView>(R.id.phoneNumberTextView).text.toString()
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("smsto:$phone")
+
+            // 문자 메시지 앱을 실행
+            startActivity(intent)
+        }
     }
 }

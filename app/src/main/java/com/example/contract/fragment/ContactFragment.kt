fd8f2f5c59f2f97a797efd77d7b6613d814153f5
dannyjoo7 <dannyjoo2003@gmail.com>
2023-09-08 21:10:04 +0900

    [REFACTOR] : 경고 문구 제거

--- app/src/main/java/com/example/contract/fragment/ContactFragment.kt
+++ app/src/main/java/com/example/contract/fragment/ContactFragment.kt
@@ -18,18 +18,11 @@ class ContactFragment : Fragment() {
 
     private lateinit var binding: FragmentContractBinding
 
-    // Fragment가 처음으로 생성될 때 호출됩니다.
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Fragment 초기화 및 데이터 로딩 등의 작업을 수행할 수 있습니다.
-    }
-
     // Fragment의 UI를 생성하고 반환합니다.
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Fragment의 레이아웃을 인플레이트하고 반환합니다.
         binding = FragmentContractBinding.inflate(inflater, container, false)
         val root = binding.root
@@ -49,32 +42,5 @@ class ContactFragment : Fragment() {
         itemTouchHelper.attachToRecyclerView(contactRv)
 
         return root
-    }
-
-    // onCreateView에서 반환된 뷰가 생성된 후 호출됩니다.
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        // onViewCreated에서 뷰에 대한 조작 및 이벤트 처리를 수행합니다.
-    }
-
-    // Fragment가 사용자에게 보여질 때 호출됩니다.
-    override fun onResume() {
-        super.onResume()
-
-        // 사용자에게 보여지는 동안 필요한 작업을 수행합니다.
-    }
-
-    // Fragment에서 필요한 자원을 해제하고 정리할 때 호출됩니다.
-    override fun onDestroyView() {
-        super.onDestroyView()
-
-        // 뷰와 관련된 자원을 해제할 수 있습니다.
-    }
-
-    // Fragment에서 사용하는 자원을 해제할 때 호출됩니다.
-    override fun onDestroy() {
-        super.onDestroy()
-
-        // 자원을 정리하고 메모리 누수를 방지할 수 있습니다.
     }
 }
--- app/src/main/java/com/example/contract/sampledata/ContactManager.kt
+++ app/src/main/java/com/example/contract/sampledata/ContactManager.kt
@@ -1,13 +1,12 @@
 package com.example.contract.sampledata
 
 import android.net.Uri
-import android.util.Log
 
 object ContactManager {
     private val contactList = ArrayList<ContactItem>()
     private var index = 0
-    private val drawableResName = "img_profile" // Drawable 리소스 이름 (파일명과 동일)
-    private val pakageName = "com.example.contract"
+    private const val drawableResName = "img_profile" // Drawable 리소스 이름 (파일명과 동일)
+    private const val pakageName = "com.example.contract"
     private val uri = Uri.parse("android.resource://$pakageName/drawable/$drawableResName")
 
     init {
@@ -32,22 +31,5 @@ object ContactManager {
 
     fun getContact(): ArrayList<ContactItem> {
         return contactList
-    }
-
-    // Logcat 으로 연락처 확인
-    fun printContact() {
-        for ((index, contact) in contactList.withIndex()) {
-            Log.d(
-                "ContactInfo",
-                "Index: $index, " +
-                        "profileImage: ${contact.profileImage}, " +
-                        "listName: ${contact.listName}, " +
-                        "groupName: ${contact.groupName}, " +
-                        "isFavorite: ${contact.isFavorite}, " +
-                        "phoneNumber: ${contact.phoneNumber} " +
-                        "email: ${contact.email}, " +
-                        "event: ${contact.event}"
-            )
-        }
     }
 }

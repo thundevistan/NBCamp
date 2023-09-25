13b02d74836e142c82c8c72df356541db085157e
KotDev99 <KotDev99@gmail.com>
2023-09-25 19:47:23 +0900

    [Design] fragment_home.xml -> 버튼 배치

--- app/src/main/java/bootcamp/sparta/disneym/ui/MainActivity.kt
+++ app/src/main/java/bootcamp/sparta/disneym/ui/MainActivity.kt
@@ -4,23 +4,36 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import bootcamp.sparta.disneym.R
 import bootcamp.sparta.disneym.databinding.ActivityMainBinding
+import bootcamp.sparta.disneym.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        fun newIntent(context: Context) : Intent = Intent(context, MainActivity::class.java)
-    }
+	companion object {
+		fun newIntent(context: Context): Intent = Intent(context, MainActivity::class.java)
+	}
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
 
-        initView()
-    }
+		initView()
 
-    private fun initView()=with(binding) {
+		// 테스트
+		setFrag()
+	}
 
-    }
+	private fun initView() = with(binding) {
+
+	}
+
+	// HomeFragment 테스트
+	private fun setFrag() {
+		supportFragmentManager.commit {
+			replace(R.id.fragmentContainerView, HomeFragment())
+			setReorderingAllowed(true)
+		}
+	}
 }

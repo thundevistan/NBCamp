6dd97c0a4a52535dc3957d8f770f48da19d136aa
SeoJungHan <seojh5939@gmail.com>
2023-09-26 16:01:07 +0900

    [Feat] : 북마크 ViewModel 더미데이터 Set & item 삭제기능

---
+++ app/src/main/java/bootcamp/sparta/disneym/viewmodel/bookmark/BookmarkViewModel.kt
@@ -0,0 +1,86 @@
+package bootcamp.sparta.disneym.viewmodel.bookmark
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import bootcamp.sparta.disneym.model.BookmarkModel
+import bootcamp.sparta.disneym.ui.bookmark.BookmarkViewType
+import bootcamp.sparta.disneym.viewmodel.detail.DetailViewModel
+
+/*
+* 작성자: 서정한
+* 내용: Bookmark 데이터와 View의 업데이트를 위한 ViewModel
+* */
+class BookmarkViewModel : ViewModel() {
+    private var _list: MutableLiveData<List<BookmarkModel>> = MutableLiveData()
+    val list get() = _list
+
+    init {
+        _list.value = initData()
+    }
+
+    // 더미데이터 init
+    private fun initData(): List<BookmarkModel> {
+        val list: MutableList<BookmarkModel> = mutableListOf()
+        for (i in 0 until 10) {
+            list.add(
+                BookmarkModel(
+                    title = "영상제목${i}",
+                    imgUrl = "https://picsum.photos/150/100",
+                    description = "영상설명영상설명영상설명영상설명${i}",
+                    datetime = "2323년 9월 2${i}일",
+                )
+            )
+        }
+        return list.toList()
+    }
+
+    // 북마크 타입 변경
+    fun updateBookmarkListType(type: BookmarkViewType) {
+        val current = list.value.orEmpty().toMutableList()
+        for (i in current.indices) {
+            current[i] = current[i].copy(viewType = type)
+        }
+        _list.value = current
+    }
+
+    // item remove
+    fun removeSelectedBookmarkItem(item: BookmarkModel) {
+        fun findIndex(item: BookmarkModel): Int? {
+            val current = list.value.orEmpty().toMutableList()
+            if (current.isEmpty()) {
+                return null
+            }
+
+            val res = current.indexOf(item)
+            if (res == -1) {
+                return null
+            }
+
+            return res
+        }
+
+        val list = list.value.orEmpty().toMutableList()
+        findIndex(item)?.let {
+            list.removeAt(it)
+        }
+        _list.value = list
+    }
+
+    // 체크박스 업데이트
+    fun updateItemChecked(position: Int, item: BookmarkModel) {
+        val current = list.value.orEmpty().toMutableList()
+        current[position] = item
+        _list.value = current
+    }
+}
+
+class BookmarkViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BookmarkViewModel() as T
+        }
+        throw IllegalArgumentException("Not Found ViewModel class.")
+    }
+}

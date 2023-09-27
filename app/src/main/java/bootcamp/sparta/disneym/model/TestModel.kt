89dfa8aed433b6c080b93e71b42052cfa2d8069d
SeoJungHan <seojh5939@gmail.com>
2023-09-27 15:29:06 +0900

    [Feat] : SharedPreference 구현완료

--- app/src/main/java/bootcamp/sparta/disneym/model/TestModel.kt
+++ app/src/main/java/bootcamp/sparta/disneym/model/TestModel.kt
@@ -1,7 +1,7 @@
 package bootcamp.sparta.disneym.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class TestModel (

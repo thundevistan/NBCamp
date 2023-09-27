2f1aeef86e304a7f900e58d10c60642f2973e16b
추민수 <devsoftychoo@gmail.com>
2023-09-27 17:54:22 +0900

    [Feat] : 가이드를 위한 TestModel 작성완료
    다른 Model을 작성할 때 참고해서 진행하시면 될 것 같습니다 :)

---
+++ app/src/main/java/bootcamp/sparta/disneym/model/TestModel2.kt
@@ -0,0 +1,28 @@
+package bootcamp.sparta.disneym.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/*
+* 추민수
+* 다른분들 작업 전 이해를 돕기위해 구성해놓은 TestModel 입니다.
+* 확장함수를 통해 디테일 페이지로 데이터를 전달할 때 매핑해주는 함수를 포함하고 있습니다.
+*/
+@Parcelize
+data class TestModel2(
+    val title: String,
+    val imgUrl: String,
+    val description: String,
+    val datetime: String,
+    val isBookmarked : Boolean
+) : Parcelable
+
+fun TestModel2.toDetailModel(): DetailModel {
+    return DetailModel(
+        title = title,
+        imgUrl = imgUrl,
+        description = description,
+        datetime = datetime,
+        isBookmarked = isBookmarked,
+    )
+}

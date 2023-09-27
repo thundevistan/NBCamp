0b724f4eff2b9e1e0b3d632f3fa0354a7aeec89c
추민수 <devsoftychoo@gmail.com>
2023-09-27 17:56:39 +0900

    [Feat] : Model 매핑
    1. 어떤 모델이 들어와도 DetailModel로 매핑되는 함수 추가
    2. DetailModel을 BookmarkModel로 매핑하는 함수 추가

--- app/src/main/java/bootcamp/sparta/disneym/model/DetailModel.kt
+++ app/src/main/java/bootcamp/sparta/disneym/model/DetailModel.kt
@@ -1,6 +1,7 @@
 package bootcamp.sparta.disneym.model
 
 import android.os.Parcelable
+import bootcamp.sparta.disneym.ui.bookmark.BookmarkViewType
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
@@ -8,5 +9,18 @@ data class DetailModel(
     val title: String,
     val imgUrl : String,
     val description : String,
-    val datetime : String
+    val datetime : String,
+    val isBookmarked : Boolean
 ) : Parcelable
+
+fun DetailModel.toBookmarkModel() : BookmarkModel{
+    return BookmarkModel(
+        title = title,
+        imgUrl = imgUrl,
+        description = description,
+        datetime = datetime,
+        isBookmarked = isBookmarked,
+        viewType = BookmarkViewType.Normal,
+        isChecked = false
+    )
+}

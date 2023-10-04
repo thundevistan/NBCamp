85c052e25418549bd36b99563d85056ed86ea218
SeoJungHan <seojh5939@gmail.com>
2023-10-04 17:25:26 +0900

    [Feat] : DetailActivity와 intent 구현 && 코드정리

--- app/src/main/java/bootcamp/sparta/disneym/ui/main/MainActivity.kt
+++ app/src/main/java/bootcamp/sparta/disneym/ui/main/MainActivity.kt
@@ -2,63 +2,82 @@ package bootcamp.sparta.disneym.ui.main
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.viewModels
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
-import androidx.viewpager2.widget.ViewPager2
 import bootcamp.sparta.disneym.R
 import bootcamp.sparta.disneym.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
+import bootcamp.sparta.disneym.model.DetailModel
+import bootcamp.sparta.disneym.ui.detail.DetailActivity
+import bootcamp.sparta.disneym.ui.viewmodel.MainSharedViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
-	companion object {
-		fun newIntent(context: Context): Intent = Intent(context, MainActivity::class.java)
-	}
+    companion object {
+        fun newIntentForSearch(
+            context: Context,
+            item: DetailModel
+        ): Intent = Intent(context, MainActivity::class.java).apply {
+            putExtra(DetailActivity.EXTRA_DETAIL, item)
+        }
+    }
 
-	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-	val viewPagerAdapter by lazy { ViewPagerAdapter(this) }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewPagerAdapter by lazy { ViewPagerAdapter(this) }
+    private val sharedViewModel: MainSharedViewModel by viewModels()
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(binding.root)
+    private val detailItem by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra(DetailActivity.EXTRA_DETAIL, DetailModel::class.java)
+        } else {
+            intent.getParcelableExtra(DetailActivity.EXTRA_DETAIL)
+        }
+    }
 
-		initView()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
 
-	}
+        initView()
+    }
 
-	private fun initView() = with(binding) {
-		// viewPager <-> tabLayout
-		tabLayoutMediator(tabLayout, viewpager)
-	}
+    private fun initView() = with(binding) {
+        // 디자인 가이드라인에서는 사용자의 터치 스크롤로 인한 화면 변경을 금지하는 것을 추천하여 추가 했습니다
+        viewpager.isUserInputEnabled = false
+        viewpager.offscreenPageLimit = viewPagerAdapter.itemCount
 
-
-	// viewPager <-> tabLayout 연결하는 함수
-	private fun tabLayoutMediator(tabLayout: TabLayout, viewPager: ViewPager2) {
-		binding.viewpager.adapter = viewPagerAdapter
-
-
-		TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-			when (position) {
-				0 -> tab.icon =
-					ResourcesCompat.getDrawable(this.resources, R.drawable.ic_home, theme)
-
-
-				1 -> tab.icon =
-					ResourcesCompat.getDrawable(this.resources, R.drawable.ic_search, theme)
-
-
-				2 -> tab.icon =
-					ResourcesCompat.getDrawable(this.resources, R.drawable.ic_download, theme)
+        // viewPager <-> tabLayout
+        binding.viewpager.adapter = viewPagerAdapter
+        TabLayoutMediator(tabLayout, viewpager) { tab, position ->
+            when (position) {
+                0 -> tab.icon =
+                    ResourcesCompat.getDrawable(resources, R.drawable.ic_home, theme)
 
 
-				3 -> tab.icon =
-					ResourcesCompat.getDrawable(this.resources, R.drawable.ic_user, theme)
-			}
-		}.attach()
+                1 -> tab.icon =
+                    ResourcesCompat.getDrawable(resources, R.drawable.ic_search, theme)
 
-		// 디자인 가이드라인에서는 사용자의 터치 스크롤로 인한 화면 변경을 금지하는 것을 추천하여 추가 했습니다
-		binding.viewpager.isUserInputEnabled = false
-	}
+
+                2 -> tab.icon =
+                    ResourcesCompat.getDrawable(resources, R.drawable.ic_download, theme)
+
+
+                3 -> tab.icon =
+                    ResourcesCompat.getDrawable(resources, R.drawable.ic_user, theme)
+            }
+        }.attach()
+
+        // Detail Fragment의 Bookmark 클릭이벤트
+        detailItem?.let {
+            sharedViewModel.updateHomeItems(it)
+        }
+    }
+
+    // 뒤로가기 막기
+    override fun onBackPressed() {
+//        super.onBackPressed()
+    }
 }

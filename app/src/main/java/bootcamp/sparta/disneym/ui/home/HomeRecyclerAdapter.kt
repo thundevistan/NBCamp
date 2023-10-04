001926fd76036c5c1c90fff2a94ab2f14f76b608
SeoJungHan <seojh5939@gmail.com>
2023-10-04 17:24:32 +0900

    [Refactor] : ListAdapter로 변경 && onItemClick 무명함수 추가

--- app/src/main/java/bootcamp/sparta/disneym/ui/home/HomeRecyclerAdapter.kt
+++ app/src/main/java/bootcamp/sparta/disneym/ui/home/HomeRecyclerAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
 import bootcamp.sparta.disneym.databinding.HomeRecyclerItemBinding
 import bootcamp.sparta.disneym.model.HomeModel
 import com.bumptech.glide.Glide
@@ -15,50 +17,90 @@ import com.bumptech.glide.Glide
  *
  * HomeFragment 하단의 scrollView 안에 위치한 recyclerView의 어댑터
  */
-class HomeRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+//class HomeRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+//
+//	private var oldItems = emptyList<HomeModel>()
+//
+//	interface ItemClick {
+//		fun onClick(view: View, position: Int)
+//	}
+//
+//	var itemClick: ItemClick? = null
+//
+//	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//		val view = HomeRecyclerItemBinding.inflate(
+//			LayoutInflater.from(parent.context),
+//			parent,
+//			false
+//		)
+//		return ViewHolder(view)
+//	}
+//
+//	override fun getItemCount(): Int {
+//		return oldItems.size
+//	}
+//
+//	override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+//		(holder as ViewHolder).setData(oldItems[position])
+//	}
+//
+//	inner class ViewHolder(itemView: HomeRecyclerItemBinding) :
+//		RecyclerView.ViewHolder(itemView.root) {
+//
+//		private val binding = itemView
+//
+//		fun setData(data: HomeModel) {
+//			Glide.with(binding.root)
+//				.load(data.imgUrl)
+//				.into(binding.homeThumbnailIv)
+//			Log.d("imgUrl", data.imgUrl)
+//		}
+//	}
+//
+//	fun setData(newList: List<HomeModel>) {
+//		val videoDiff = DiffUtil(oldItems, newList)
+//		val diff = DiffUtil.calculateDiff(videoDiff)
+//		oldItems = newList
+//		diff.dispatchUpdatesTo(this)
+//	}
+//}
 
-	private var oldItems = emptyList<HomeModel>()
+class HomeRecyclerAdapter(
+	private val onItemClicked: (HomeModel) -> Unit,
+) : ListAdapter<HomeModel, HomeRecyclerAdapter.Holder>(
+	object: DiffUtil.ItemCallback<HomeModel>() {
+		override fun areItemsTheSame(oldItem: HomeModel, newItem: HomeModel): Boolean {
+			return oldItem.imgUrl == newItem.imgUrl
+		}
 
-	interface ItemClick {
-		fun onClick(view: View, position: Int)
+		override fun areContentsTheSame(oldItem: HomeModel, newItem: HomeModel): Boolean {
+			return oldItem == newItem
+		}
+	}
+) {
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+		val view = HomeRecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+		return Holder(view, onItemClicked)
 	}
 
-	var itemClick: ItemClick? = null
-
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-		val view = HomeRecyclerItemBinding.inflate(
-			LayoutInflater.from(parent.context),
-			parent,
-			false
-		)
-		return ViewHolder(view)
+	override fun onBindViewHolder(holder: Holder, position: Int) {
+		holder.onBind(getItem(position))
 	}
 
-	override fun getItemCount(): Int {
-		return oldItems.size
-	}
-
-	override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-		(holder as ViewHolder).setData(oldItems[position])
-	}
-
-	inner class ViewHolder(itemView: HomeRecyclerItemBinding) :
-		RecyclerView.ViewHolder(itemView.root) {
-
-		private val binding = itemView
-
-		fun setData(data: HomeModel) {
+	class Holder(
+		private val binding: HomeRecyclerItemBinding,
+		private val onItemClicked: (HomeModel) -> Unit
+	): RecyclerView.ViewHolder(binding.root) {
+		fun onBind(item: HomeModel)=with(binding) {
 			Glide.with(binding.root)
-				.load(data.imgUrl)
+				.load(item.imgUrl)
 				.into(binding.homeThumbnailIv)
-			Log.d("imgUrl", data.imgUrl)
+
+			// 클릭이벤트
+			itemView.setOnClickListener {
+				onItemClicked(item)
+			}
 		}
 	}
 
-	fun setData(newList: List<HomeModel>) {
-		val videoDiff = DiffUtil(oldItems, newList)
-		val diff = DiffUtil.calculateDiff(videoDiff)
-		oldItems = newList
-		diff.dispatchUpdatesTo(this)
-	}
 }

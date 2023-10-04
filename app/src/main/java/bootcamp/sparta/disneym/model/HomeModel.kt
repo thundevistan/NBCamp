352636efc4be4f168bb125afc212ffeffdf90a61
SeoJungHan <seojh5939@gmail.com>
2023-10-04 17:17:01 +0900

    [Refactor] : 모델클래스 id 값 nullable로 변경 && HomeModel.toDetailModel 확장함수 추

--- app/src/main/java/bootcamp/sparta/disneym/model/HomeModel.kt
+++ app/src/main/java/bootcamp/sparta/disneym/model/HomeModel.kt
@@ -6,22 +6,32 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class HomeModel(
-	val id: String,
-	val title: String,
-	val description: String,
-	val imgUrl: String,
-	val datetime: String,
-	val isBookmarked: Boolean = false,
+    val id: String? = null,
+    val title: String,
+    val description: String,
+    val imgUrl: String,
+    val datetime: String,
+    val isBookmarked: Boolean = false,
 ) : Parcelable
 
 fun HomeModel.toBookmarkModel(): BookmarkModel =
-	BookmarkModel(
-		id = id,
-		title = title,
-		imgUrl = imgUrl,
-		description = description,
-		datetime = datetime,
-		isBookmarked = isBookmarked,
-		viewType = BookmarkViewType.Normal,
-		isChecked = false
-	)
+    BookmarkModel(
+        id = id,
+        title = title,
+        imgUrl = imgUrl,
+        description = description,
+        datetime = datetime,
+        isBookmarked = isBookmarked,
+        viewType = BookmarkViewType.Normal,
+        isChecked = false
+    )
+
+fun HomeModel.toDetailModel(): DetailModel =
+    DetailModel(
+        id = id,
+        title = title,
+        imgUrl = imgUrl,
+        description = description,
+        datetime = datetime,
+        isBookmarked = isBookmarked,
+    )
